495c532916a2d99f5dc1fe4256ce660545aa5648
Stefan Reck <stefan.reck@fau.de>
2020-01-30 12:30:33 +0100

    add new concatenate script WIP

---
+++ tests/test_concatenate.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+import h5py
+import orcasong.tools.concatenate as conc
+
+__author__ = 'Stefan Reck'
+
+
+class TestModules(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
